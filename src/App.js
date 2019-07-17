6227da02804ae6f0b39bdf0e31e5f8fc9d167991
brettmadrid <brettmadrid9@gmail.com>
2019-07-17 12:26:37 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  // Initialize state to hold the image URL
+  const [nasaPic, setnasaPic] = useState("");
+  useEffect(() => {
+    // This axios GET request will return a single image
+    axios
+      .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2019-07-15")
+      // Which we then set to state
+      .then(res => {
+        console.log('res data', res.data)
+        setnasaPic(res.data.hdurl)
+      })
+      // Always include error handling
+      .catch(err => console.log(err));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Nasa Pic</h1>
+      <img src={nasaPic} alt="a Nasa pic" />
     </div>
   );
 }
